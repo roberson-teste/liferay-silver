beae765d6bf8c7ab93bd989193968cf9ff7c2d5f
Unknown <robersonccgomes@gmail.com>
2016-12-08 17:40:27 -0200

    fail test

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example;
 
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,11 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void myTest() {
+		Assert.fail();
 	}
 
 }
